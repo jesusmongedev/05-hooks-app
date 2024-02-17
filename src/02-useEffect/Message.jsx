3752da0125df361e5d19113f7f85fb750a9978a0
Jesús Andrés Monge <jesus_jaml@outlook.com>
2024-02-17 16:36:06 -0500

    [UPD] clean event listeners avoid memory leak

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,10 +1,19 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 function Message () {
+  const [coords, setCoords] = useState({
+    x: 0,
+    y: 0
+  })
   useEffect(() => {
-    console.log('Mounted')
+    const mouseMove = (e) => {
+      const { x, y } = e
+      setCoords({ x, y })
+    }
+    window.addEventListener('mousemove', mouseMove)
 
     return () => {
+      window.removeEventListener('mousemove', mouseMove)
       console.log('UnMounted')
     }
   }, [])
@@ -12,6 +21,9 @@ function Message () {
   return (
     <>
       Usuario existente
+      {
+        JSON.stringify(coords, null, 2)
+      }
     </>
   )
 }
